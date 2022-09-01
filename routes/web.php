9cb728fd2dfbcfa86e5c8336d1bdbec791ac6e50
Azeem <59115414+azeemade@users.noreply.github.com>
2022-09-01 11:05:37 +0100

    update to route

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ImageController::class, 'index'])->name('overview');
 
 Route::post('/image/upload', [ImageController::class, 'convert'])->name('upload');
-
-Route::get('/image/download', [ImageController::class, 'download'])->name('download');
